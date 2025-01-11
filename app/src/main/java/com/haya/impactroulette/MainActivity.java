3f2d8a6f0ba097c45285af572b10c3303aa2c61c
João Pedro Barbosa <joppemdesu@gmail.com>
2025-01-10 21:23:20 -0400

    aparência da primeira tela criada

--- app/src/main/java/com/haya/impactroulette/MainActivity.java
+++ app/src/main/java/com/haya/impactroulette/MainActivity.java
@@ -1,6 +1,7 @@
 package com.haya.impactroulette;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imgPaimon, imgRoleta;
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        imgRoleta = findViewById(R.id.img_roleta);
+
+        imgRoleta.animate()
+                .rotationBy(360f)
+                .setDuration(2000)
+                .setInterpolator(null)
+                .withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        imgRoleta.animate().rotationBy(360f).setDuration(2000).withEndAction(this).start();
+                    }
+                }).start();
     }
 }
